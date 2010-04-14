661fa236d308fd73de82bcb0f6e222bba837efd7
Tomas Linhart <SneakerXZ@gmail.com>
2010-04-14 14:44:16 +0200

    Created a new WPF Project.

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace BitTorrent_client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
