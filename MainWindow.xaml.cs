cbcc8b85e7fbc53b09be4bc7d87010cb7e5bd0d3
Alexey Koloskov <schyzotrop@gmail.com>
2010-04-15 14:31:17 +0200

    Layout implemented + torrent listing, filtering & updating implemented

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,27 +1,100 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace BitTorrent_client
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private readonly DispatcherTimer _timer;
+        private TorrentStatus[] _currentFilter;
+
+        public ObservableCollection<TorrentData> TorrentCollection { get; private set; }
+        public ObservableCollection<TorrentData> FilteredCollection { get; private set; }
+
         public MainWindow()
         {
+            TorrentCollection = new ObservableCollection<TorrentData>();
+            FilteredCollection = new ObservableCollection<TorrentData>();
             InitializeComponent();
+            _currentFilter = new[] { TorrentStatus.Finished, TorrentStatus.Paused };
+            TorrentCollection.CollectionChanged += OnTorrentCollectionChanged;
+
+            listBox.SelectionChanged += OnListBoxSelectionChanged;
+
+            #region TESTING
+            _timer = new DispatcherTimer { Interval = new TimeSpan(0, 0, 0, 3, 0) };
+            _timer.Tick += OnTimerTick;
+            _timer.Start();
+            SetTestData();
+            #endregion
+        }
+        #region FOR TESTING ONLY
+        private void SetTestData()
+        {
+            TorrentCollection.Add(new TorrentData { TorrentName = "test", Status = TorrentStatus.Finished });
+            TorrentCollection.Add(new TorrentData { TorrentName = "test", Status = TorrentStatus.Paused });
+            TorrentCollection.Add(new TorrentData { TorrentName = "test", Status = TorrentStatus.Stopped });
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            TorrentCollection.Add(new TorrentData { TorrentName = "hi", Status = TorrentStatus.Paused });
+            TorrentCollection.First().TorrentName = "qwer";
+            TorrentCollection.First().Priority = 3;
+            TorrentCollection.First().Status = TorrentStatus.Finished;
+            TorrentCollection.First().Progress = 7.5;
+
+        }
+        #endregion
+
+        private void FilterCollection()
+        {
+            FilteredCollection.Clear();
+            foreach (var torrentData in from torrentData in TorrentCollection
+                                        from torrentStatus in _currentFilter
+                                        where torrentData.Status == torrentStatus
+                                        select torrentData)
+            {
+                FilteredCollection.Add(torrentData);
+            }
+        }
+
+        void OnTorrentCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            FilterCollection();
+        }
+
+        void OnListBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listBoxItem = (((ListBox)sender).SelectedItem as ListBoxItem);
+            if (listBoxItem != null)
+            {
+                switch (listBoxItem.Content.ToString())
+                {
+                    case "Downloading":
+                        _currentFilter = new[] { TorrentStatus.Paused };
+                        break;
+                    case "Completed":
+                        _currentFilter = new[] { TorrentStatus.Paused };
+                        break;
+                    case "Active":
+                        _currentFilter = new[] { TorrentStatus.Paused };
+                        break;
+                    case "Inactive":
+                        _currentFilter = new[] { TorrentStatus.Paused };
+                        break;
+                    default:
+                        _currentFilter = new[] { TorrentStatus.Finished, TorrentStatus.Paused, TorrentStatus.Stopped };
+                        break;
+                }
+            }
+            FilterCollection();
         }
     }
 }
---
+++ TorrentData.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace BitTorrent_client
+{
+    public enum TorrentStatus
+    {
+        Paused = 0,
+        Finished = 1,
+        Stopped = 2
+    } ;
+
+    public class TorrentData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        private string _torrentName;
+        private int _priority;
+        private int _size;
+        private double _progress;
+        private TorrentStatus _status;
+        private int _seeds;
+        private int _peers;
+        private double _downloadSpeed;
+        private double _uploadSpeed;
+
+
+        public string TorrentName
+        {
+            get { return _torrentName; }
+            set
+            {
+                _torrentName = value;
+                OnPropertyChanged("TorrentName");
+            }
+        }
+
+        public int Priority
+        {
+            get { return _priority; }
+            set
+            {
+                _priority = value;
+                OnPropertyChanged("Priority");
+            }
+        }
+
+        public double Progress
+        {
+            get { return _progress; }
+            set
+            {
+                _progress = value;
+                OnPropertyChanged("Progress");
+            }
+        }
+
+        public int Size
+        {
+            get { return _size; }
+            set
+            {
+                _size = value;
+                OnPropertyChanged("Size");
+            }
+        }
+
+        public TorrentStatus Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                OnPropertyChanged("Status");
+            }
+        }
+
+        public int Seeds
+        {
+            get { return _seeds; }
+            set
+            {
+                _seeds = value;
+                OnPropertyChanged("Seeds");
+            }
+        }
+
+        public int Peers
+        {
+            get { return _peers; }
+            set
+            {
+                _peers = value;
+                OnPropertyChanged("Peers");
+            }
+        }
+
+        public double DownloadSpeed
+        {
+            get { return _downloadSpeed; }
+            set
+            {
+                _downloadSpeed = value;
+                OnPropertyChanged("DownloadSpeed");
+            }
+        }
+
+        public double UploadSpeed
+        {
+            get { return _uploadSpeed; }
+            set
+            {
+                _uploadSpeed = value;
+                OnPropertyChanged("UploadSpeed");
+            }
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            var propertyChangedEventHandler = PropertyChanged;
+            if (propertyChangedEventHandler != null)
+            {
+                propertyChangedEventHandler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
